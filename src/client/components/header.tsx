c859f6109f35b901c41dcbca886ff9acd301541e
minu <made_power@naver.com>
2022-04-16 13:34:01 +0900

    aa

---
+++ src/client/components/header.tsx
@@ -0,0 +1,58 @@
+import styled from '@emotion/styled';
+import React from 'react';
+
+const Copntainer = styled.div`
+  width :100%;
+  height : 80px;
+  background-color: red;
+  margin :auto;
+  >li{
+      float:left;
+      padding: 20px;
+  }
+  > span {
+    color: rgba(0, 0, 0, 0.87);
+    font-size: 0.92857143em;
+    font-weight: 700;
+  }
+
+  > input {
+    box-sizing: border-box;
+    margin: 10px 0px;
+    outline: 0;
+    line-height: 1.21428571em;
+    padding: 10px;
+    font-size: 1em;
+    background: #fff;
+    border: 1px solid rgba(34, 36, 38, 0.15);
+    color: rgba(0, 0, 0, 0.87);
+    border-radius: 4px;
+    width: 100%;
+    vertical-align: top;
+  }
+`;
+
+type Props = {
+  label?: string;
+  placeholder: string;
+  value?: string;
+  disabled?: boolean;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+function Input({
+  label,
+  placeholder,
+  value,
+  disabled = false,
+  onChange,
+}: Props) {
+  return (
+    <Copntainer>
+      {label && <span>{label}</span>}
+      <input {...{ placeholder, value, onChange, disabled }} />
+    </Copntainer>
+  );
+}
+
+export default Input;
